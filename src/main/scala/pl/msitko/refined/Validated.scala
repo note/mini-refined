1d0e4b06d49b2e7a1ae90193903fab5347644c62
Michal Sitko <pierwszy1@gmail.com>
2021-04-30 11:16:18 +0200

    Introduce Refined type

--- src/main/scala/pl/msitko/refined/Validated.scala
+++ src/main/scala/pl/msitko/refined/Validated.scala
@@ -6,23 +6,34 @@ import scala.compiletime.{codeOf, constValue, erasedValue, error}
 import pl.msitko.refined.ValidateExpr
 import pl.msitko.refined.ValidateExpr._
 
-trait Validated[E <: ValidateExpr]
+object RefinedLift:
+  implicit inline def mkValidatedInt[V <: Int with Singleton, E <: ValidateExpr](v: V): Refined[V, E] =
+    inline erasedValue[E] match
+      case _: LowerThan[t] =>
+        inline erasedValue[V] < erasedValue[t] match
+          case _: true => Refined.unsafeApply(v)
+          case _: false =>
+            error(
+              "Validation failed: " + constValue[ToString[V]] + " < " + constValue[ToString[t]]
+            ) // ${erasedValue[V].toString} < ${erasedValue[t].toString}
+      case _: GreaterThan[t] =>
+        inline erasedValue[V] > erasedValue[t] match
+          case _: true  => Refined.unsafeApply(v)
+          case _: false => error("Validation failed: " + constValue[ToString[V]] + " < " + constValue[ToString[t]])
+      case _: And[a, b] =>
+        inline mkValidatedInt[V, a](v) match
+          case _: Refined[_, _] =>
+            inline mkValidatedInt[V, b](v) match
+              case _: Refined[_, _] => Refined.unsafeApply(v)
 
-implicit inline def mkValidated[V <: Int with Singleton, E <: ValidateExpr](v: V): Validated[E] =
-  inline erasedValue[E] match
-    case _: LowerThan[t] =>
-      inline erasedValue[V] < erasedValue[t] match
-        case _: true => new Validated[E] {}
-        case _: false =>
-          error(
-            "Validation failed: " + constValue[ToString[V]] + " < " + constValue[ToString[t]]
-          ) // ${erasedValue[V].toString} < ${erasedValue[t].toString}
-    case _: GreaterThan[t] =>
-      inline erasedValue[V] > erasedValue[t] match
-        case _: true  => new Validated[E] {}
-        case _: false => error("Validation failed: " + constValue[ToString[V]] + " < " + constValue[ToString[t]])
-    case _: And[a, b] =>
-      inline mkValidated[V, a](v) match
-        case _: Validated[_] =>
-          inline mkValidated[V, b](v) match
-            case _: Validated[_] => new Validated[E] {}
+opaque type Refined[+Underlying, ValidateExpr] = Underlying
+object Refined:
+  // We cannot simply `implicit inline def mk...(): Refined` because inline and opaque types do not compose
+  // Read about it here: https://github.com/lampepfl/dotty/issues/6802
+  private [refined] def unsafeApply[V <: Int with Singleton, E <: ValidateExpr](i: Int): V Refined E = ???
+
+
+object Example:
+  @main def main(): Unit =
+    import RefinedLift._
+    val a: Int Refined GreaterThan[10] = RefinedLift.mkValidatedInt[16, GreaterThan[10]](16)
--- src/test/scala/SomeSpec.scala
+++
@@ -1,7 +0,0 @@
-package example
-
-class SomeSpec extends munit.FunSuite {
-  test("2 + 2 should equal 4") {
-    assertEquals(2 + 2, 4)
-  }
-}
