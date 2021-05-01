4ad81618ec66ff7546f466d8cf27a810f6a5082e
Michal Sitko <pierwszy1@gmail.com>
2021-05-01 11:33:37 +0200

    Refactoring

--- src/main/scala/pl/msitko/refined/Validated.scala
+++ src/main/scala/pl/msitko/refined/Validated.scala
@@ -8,32 +8,38 @@ import pl.msitko.refined.ValidateExpr._
 
 object RefinedLift:
   implicit inline def mkValidatedInt[V <: Int with Singleton, E <: ValidateExpr](v: V): Refined[V, E] =
+    inline if checkPredicate[V, E]
+      then Refined.unsafeApply(v)
+      else error("Validation failed")
+
+  transparent inline def checkPredicate[V <: Int with Singleton, E <: ValidateExpr]: Boolean =
     inline erasedValue[E] match
       case _: LowerThan[t] =>
         inline erasedValue[V] < erasedValue[t] match
-          case _: true => Refined.unsafeApply(v)
+          case _: true => true
           case _: false =>
             error(
               "Validation failed: " + constValue[ToString[V]] + " < " + constValue[ToString[t]]
             ) // ${erasedValue[V].toString} < ${erasedValue[t].toString}
       case _: GreaterThan[t] =>
         inline erasedValue[V] > erasedValue[t] match
-          case _: true  => Refined.unsafeApply(v)
+          case _: true  => true
           case _: false => error("Validation failed: " + constValue[ToString[V]] + " < " + constValue[ToString[t]])
       case _: And[a, b] =>
-        inline mkValidatedInt[V, a](v) match
-          case _: Refined[_, _] =>
-            inline mkValidatedInt[V, b](v) match
-              case _: Refined[_, _] => Refined.unsafeApply(v)
+        inline checkPredicate[V, a] match
+          case true =>
+            inline checkPredicate[V, b] match
+              case true => true
 
 opaque type Refined[+Underlying, ValidateExpr] = Underlying
 object Refined:
   // We cannot simply `implicit inline def mk...(): Refined` because inline and opaque types do not compose
   // Read about it here: https://github.com/lampepfl/dotty/issues/6802
-  private [refined] def unsafeApply[V <: Int with Singleton, E <: ValidateExpr](i: Int): V Refined E = ???
+  private [refined] def unsafeApply[V <: Int with Singleton, E <: ValidateExpr](i: V): V Refined E = i
 
 
 object Example:
   @main def main(): Unit =
     import RefinedLift._
     val a: Int Refined GreaterThan[10] = RefinedLift.mkValidatedInt[16, GreaterThan[10]](16)
+    val b: Int Refined And[GreaterThan[10], LowerThan[20]] = RefinedLift.mkValidatedInt[16, And[GreaterThan[10], LowerThan[20]]](16)
