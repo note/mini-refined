1774da1ca142b6eadc7a09913d30a27dc9f23a9e
Michal Sitko <pierwszy1@gmail.com>
2021-06-12 19:34:27 +0200

    wip

--- src/test/scala/pl/msitko/refined/runtime/ValidateStringSpec.scala
+++ src/test/scala/pl/msitko/refined/runtime/ValidateStringSpec.scala
@@ -2,6 +2,7 @@ package pl.msitko.refined.runtime
 
 import pl.msitko.refined.Refined
 import pl.msitko.refined.auto._
+import pl.msitko.refined.compiletime.ValidateExprString.{EndsWith, StartsWith}
 
 class ValidateStringSpec extends munit.FunSuite {
   test("should work for StartsWith") {
@@ -17,5 +18,18 @@ class ValidateStringSpec extends munit.FunSuite {
     type Pred = Or[StartsWith["abc"], EndsWith["xyz"]]
     val correctValues = List("abc", "abcd", "xyz", "fxyz")
     assert(clue(correctValues.map(Refined.refineV[Pred](_))).forall(_.isRight))
+  }
+  test("should work for Or - negative cases") {
+    type Pred = Or[StartsWith["abc"], EndsWith["xyz"]]
+    val incorrectValues = List("abd", "abyz")
+    assert(clue(incorrectValues.map(Refined.refineV[Pred](_))).forall(_.isLeft))
+  }
+  test("should work for nested Or(And, And)") {
+    type Pred = Or[And[StartsWith["abc"], EndsWith["xyz"]], And[StartsWith["123"], EndsWith["abc"]]]
+    val correctValues = List("abcxyz", "abcdefxyz", "123abc", "123ppppabc")
+    val incorrectValues = List("abxyz", "abcyz", "pppooo", "123bc", "12abc", "12aaabc")
+
+    assert(clue(correctValues.map(Refined.refineV[Pred](_))).forall(_.isRight))
+    assert(clue(incorrectValues.map(Refined.refineV[Pred](_))).forall(_.isLeft))
   }
 }
